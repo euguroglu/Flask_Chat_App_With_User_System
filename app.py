57120b5ccbb9de75e51b16722ba9855f947e502d
euguroglu <e.uguroglu@yahoo.com>
2021-01-01 19:31:39 +0300

    add flask-wtf form registration page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from form import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret1'
@@ -6,7 +7,11 @@ app.config['SECRET_KEY'] = 'secret1'
 @app.route("/",methods = ['GET','POST'])
 def index():
 
-    return render_template('index.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return "Great success!"
+
+    return render_template('index.html',form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
